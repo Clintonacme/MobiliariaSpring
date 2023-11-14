f8a03b745693c9e44ddcc7bab1e30d649b51b293
Carlos <cmag3313@gmail.com>
2023-11-13 22:42:59 -0500

    Agregue 2 mantenedores

---
+++ src/main/java/com/muebleria/demo/repository/iTipoRepository.java
@@ -0,0 +1,11 @@
+package com.muebleria.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.muebleria.demo.model.Tipo;
+
+
+public interface iTipoRepository extends JpaRepository<Tipo, Integer> {
+	//Metodos Crud
+
+}
